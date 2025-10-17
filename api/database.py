17d7af97e74012d70665546a7ba2dc21ddde6cab
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-18 01:17:34 +0300

    Update database.py

--- api/database.py
+++ api/database.py
@@ -1,13 +1,16 @@
 from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import ssl
 
 from api.config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL)
+ssl_ctx = ssl.create_default_context()
+engine = create_async_engine(DATABASE_URL, connect_args={"ssl": ssl_ctx})
 
 async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
     pass
+
