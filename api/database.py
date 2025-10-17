14fe9de22345bd599dfddb9df30ed1117cd30237
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-18 01:05:08 +0300

    Revert to old (2)

--- api/database.py
+++ api/database.py
@@ -1,23 +1,13 @@
-import ssl
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
 from api.config import DATABASE_URL
 
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
+engine = create_async_engine(DATABASE_URL)
 
-engine = create_async_engine(
-    DATABASE_URL,
-    connect_args={"ssl": ssl_context},
-    pool_recycle=1800,
-)
+async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-async_session_maker = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
 
 class Base(DeclarativeBase):
     pass
