a685a4fb40c28c61fb86df3d5eaac83506cc31e8
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-18 00:21:33 +0300

    Update database.py
    Add SSL to DB

--- api/database.py
+++ api/database.py
@@ -2,12 +2,17 @@ from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+import ssl
+
 from api.config import DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(
+    DATABASE_URL,
+    connect_args={"ssl": ssl_context})
 
 async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
     pass
+
