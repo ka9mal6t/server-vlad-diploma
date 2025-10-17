ab90bcc9fdfe3cbd7676910fbddea8eac42ab2c5
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-18 00:39:44 +0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -1,5 +1,4 @@
-from typing import Literal
-
+# api/config.py
 from dotenv import load_dotenv
 import os
 
@@ -11,20 +10,20 @@ DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
 
+# Берем URL либо из Render, либо формируем вручную (для локалки)
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+elif DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+
 SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = os.getenv("SMTP_PORT")
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 REDIS_HOST = os.getenv("REDIS_HOST")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ENCRYPT_KEY = os.getenv("ENCRYPT_KEY")
-# from secrets import token_bytes
-# from base64 import b64encode
-# print(b64encode(token_bytes(32)).decode())
-
 ALGORITHM = os.getenv("ALGORITHM")
-
