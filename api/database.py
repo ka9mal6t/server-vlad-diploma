e89b8466734f24c8d896dbefcb9ea81a20591c5d
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-18 00:26:27 +0300

    Update database.py
    Update SSL to DB

--- api/database.py
+++ api/database.py
@@ -6,13 +6,23 @@ import ssl
 
 from api.config import DATABASE_URL
 
+# Создаем SSL контекст
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
 engine = create_async_engine(
-    DATABASE_URL,
-    connect_args={"ssl": ssl_context})
+    DB_URL,
+    connect_args={"ssl": ssl_context},
+)
 
-async_session_maker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
+async_session_maker = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 class Base(DeclarativeBase):
     pass
+
 
