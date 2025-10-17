db15661f82f4e231852142bc3244681c66273cf3
Volodymyr <125738766+ka9mal6t@users.noreply.github.com>
2025-10-17 23:29:24 +0300

    Update config
    Update SSL connection to DB

--- api/config.py
+++ api/config.py
@@ -16,7 +16,7 @@ SMTP_PORT = os.getenv("SMTP_PORT")
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASS = os.getenv("SMTP_PASS")
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 
 REDIS_HOST = os.getenv("REDIS_HOST")
 
@@ -25,4 +25,5 @@ ENCRYPT_KEY = os.getenv("ENCRYPT_KEY")
 # from secrets import token_bytes
 # from base64 import b64encode
 # print(b64encode(token_bytes(32)).decode())
+
 ALGORITHM = os.getenv("ALGORITHM")
